874d17dfc29e5a5c7378e25f1fd393d6b6f9f4b6
xuyanmei <2521177848@qq.com>
2021-06-20 19:38:17 +0800

    格式化

--- src/service/cgiService.js
+++ src/service/cgiService.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { Message } from 'element-ui';
 
-axios.interceptors.request.use((config)=>{
+axios.interceptors.request.use((config) => {
     return config;
-}, (err)=>{
+}, (err) => {
     Message.error({ message: err });
 })
 
-axios.interceptors.response.use((success)=>{
+axios.interceptors.response.use((success) => {
     return success.data;
-}, (err)=>{
+}, (err) => {
     if (err.response) {
         console.log(err.response);
     } else {
@@ -17,7 +17,7 @@ axios.interceptors.response.use((success)=>{
     }
 })
 
-export const getOftenGoods = (params)=>{
+export const getOftenGoods = (params) => {
     return axios({
         url: 'https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/oftenGoods',
         method: 'get',
@@ -25,7 +25,7 @@ export const getOftenGoods = (params)=>{
     })
 }
 
-export const getTypeGoods = (params)=>{
+export const getTypeGoods = (params) => {
     return axios({
         url: 'https://www.easy-mock.com/mock/5b8b30dbf032f03c5e71de7f/kuaican/typeGoods',
         method: 'get',
